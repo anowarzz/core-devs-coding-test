d235b5075fb0bd7703c7100773f86d58e8c74b19
anowarzz <anowarhosen444@gmail.com>
2023-04-03 23:02:08 +0600

    changed the countdown ending time to future time and added proper commments for each steps

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 
 export default function Counter() {
+
+// Calculating the total milliseconds for each second, minute, hour, and day
+  const sec = 1000,
+  min = sec * 60,
+  hour = min * 60,
+  day = hour * 24;
+
+  // State to store all the values of the clock - seconds, minutes, hours and days
   let [clockData, setClockData] = useState({
     dd: 0,
     hh: 0,
@@ -8,29 +16,35 @@ export default function Counter() {
     ss: 0
   });
 
-  useEffect(() => {
-    const sec = 1000,
-      min = sec * 60,
-      hour = min * 60,
-      day = hour * 24;
 
+  // useEffect hook to run the code once when the component mounts every time
+  useEffect(() => {
+
+
+ // Set Interval function to run the clock in every one second 
     let clockInterval = setInterval(() => {
-      const end = new Date("Jul 05, 2023 12:00:00").getTime();
+ 
+// Getting the current time and the time when the count down will be finished (in milliseconds)
+      const end = new Date("April 15, 2023 12:00:00").getTime();
       const current = new Date().getTime();
    
-      
+// Calculating the total remaining time (in milliseconds) between current and the ending time  
       const remaining = end-current;
 
+
+//Converting the total remaining milliseconds into days, hours, minute and seconds
       let dd = Math.floor(remaining / day);
       let hh = Math.floor((remaining % day) / hour);
       let mm = Math.floor((remaining % hour) / min);
       let ss = Math.floor((remaining % min) / sec);
 
+// updating the clock data with new time values 
       setClockData(previous => {
         return { ...previous, dd, hh, mm, ss };
       });
     }, 1000);
 
+// Cleanup function to clear the interval 
     return () => {
       clearInterval(clockInterval);
     }
